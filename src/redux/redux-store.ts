6f1167e5f489da27fcb9079c99f6f0425df25469
vianat <vianatdev@gmail.com>
2021-08-10 14:16:56 -0700

    91 add chrome extensions settintg in store

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import usersReducer from "./users-reducer";
@@ -17,8 +17,15 @@ let reducers = combineReducers({
 })
 
 export type stateAllType = ReturnType<typeof reducers>
+// v1 default redux store
+// let store = createStore(reducers, applyMiddleware(thunkMiddleware));
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+// v2 store for web extension with, it can be removed if you use v1
+// @ts-ignore
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export const store = createStore(reducers,  composeEnhancers(applyMiddleware(thunkMiddleware)));
+// v2
+
 
 // @ts-ignore
 window.store = store;
